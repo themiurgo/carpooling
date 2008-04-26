898aab91e19e16b01013c2194415f000391daaee
Daniele Marletta <danielemar86@gmail.com>
2008-04-26 18:26:30 +0000

    Aggiunte funzionalità, ma sono ancora in una fase di 'work in progress'
    git-svn-id: file:///Volumes/Dati/docs/Uni/Repos/carpooling@21 843fd666-81f2-4710-8602-7759cdb5ad05

---
+++ script/tragittoValidate.js
@@ -0,0 +1,39 @@
+var presenzaTesto = {validMessage : " ",onlyOnBlur:true};
+
+var giorno = {minimum:1,maximum:31,
+   notANumberMessage: "Errore", notAnIntegerMessage: "Errore",
+   tooLowMessage:"Errore",tooHighMessage:"Errore"};
+var mese = {minimum:1,maximum:12,
+   notANumberMessage: "Errore", notAnIntegerMessage: "Errore",
+   tooLowMessage:"Errore",tooHighMessage:"Errore"};
+var anno = {minimum:1900,maximum:2050,
+   notANumberMessage: "Errore", notAnIntegerMessage: "Errore",
+   tooLowMessage:"Errore",tooHighMessage:"Errore"};
+var numPass = {minimum:1,maximum:7,
+   notANumberMessage: "Errore", notAnIntegerMessage: "Errore",
+   tooLowMessage:"Non valido",tooHighMessage:"Sei Sicuro?"};
+var soldi = {minimum:0,
+   notANumberMessage: "Errore", notAnIntegerMessage: "Errore",
+   tooLowMessage:"Non valido"};
+
+var gPartenza=new LiveValidation('gPartenza',presenzaTesto);
+var mPartenza=new LiveValidation('mPartenza',presenzaTesto);
+var aPartenza=new LiveValidation('aPartenza',presenzaTesto);
+var oraPartenza=new LiveValidation('oraPartenza',presenzaTesto);
+var durata=new LiveValidation('durata',presenzaTesto);
+var passeggeri=new LiveValidation('passeggeri',presenzaTesto);
+var spese=new LiveValidation('spese',presenzaTesto);
+
+gPartenza.add(Validate.Presence, {failureMessage:"Vuoto"});
+gPartenza.add( Validate.Numericality, giorno);
+mPartenza.add(Validate.Presence, {failureMessage:"Vuoto"});
+mPartenza.add( Validate.Numericality, mese);
+aPartenza.add(Validate.Presence, {failureMessage:"Vuoto"});
+aPartenza.add( Validate.Numericality, anno);
+
+oraPartenza.add(Validate.Presence, {failureMessage:"Vuoto"});
+durata.add(Validate.Presence, {failureMessage:"Vuoto"});
+passeggeri.add(Validate.Presence, {failureMessage:"Vuoto"});
+passeggeri.add(Validate.Numericality, numPass);
+spese.add(Validate.Presence, {failureMessage:"Vuoto"});
+spese.add(Validate.Numericality, soldi);

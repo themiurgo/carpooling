56b333496f0867e4257f076d9a26cb2fb4e9af72
Daniele Marletta <danielemar86@gmail.com>
2008-05-10 15:02:13 +0000

    validazione in progress
    git-svn-id: file:///Volumes/Dati/docs/Uni/Repos/carpooling@71 843fd666-81f2-4710-8602-7759cdb5ad05

--- script/iscrizioneValidate.js
+++ script/iscrizioneValidate.js
@@ -15,44 +15,48 @@ var nome=new LiveValidation('nome',{
    validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'nomeLabel'});
 var cognome=new LiveValidation('cognome',{
    validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'cognomeLabel'});
-var dBorn=new LiveValidation('giornoNascita',{
-   insertAfterWhatNode:'nascitaLabel',validMessage: ' ',onlyOnBlur:true});
-var dBorn=new LiveValidation('meseNascita',{
-   insertAfterWhatNode:'nascitaLabel',validMessage: ' ',onlyOnBlur:true});
-var dBorn=new LiveValidation('annoNascita',{
-   insertAfterWhatNode:'nascitaLabel',validMessage: ' ',onlyOnBlur:true});
-var dDrive=new LiveValidation('giornoPatente',options);
-var mDrive=new LiveValidation('mesePatente',options);
-var yDrive=new LiveValidation('annoPatente',options);
+//~ var dBorn=new LiveValidation('giornoNascita',{
+   //~ insertAfterWhatNode:'nascitaLabel',validMessage: ' ',onlyOnBlur:true});
+//~ var dBorn=new LiveValidation('meseNascita',{
+   //~ insertAfterWhatNode:'nascitaLabel',validMessage: ' ',onlyOnBlur:true});
+//~ var dBorn=new LiveValidation('annoNascita',{
+   //~ insertAfterWhatNode:'nascitaLabel',validMessage: ' ',onlyOnBlur:true});
+//~ var dDrive=new LiveValidation('giornoPatente',options);
+//~ var mDrive=new LiveValidation('mesePatente',options);
+//~ var yDrive=new LiveValidation('annoPatente',options);
 var email=new LiveValidation('email',{
    validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'emailLabel'});
-var user=new LiveValidation('user',options);
-var psw=new LiveValidation('psw',options);
-var psw2=new LiveValidation('psw2',options);
-var citta=new LiveValidation('citta',options);
+var user=new LiveValidation('user',{
+   validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'userLabel'});
+var psw=new LiveValidation('psw',{
+   validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'pswLabel'});
+var psw2=new LiveValidation('psw2',{
+   validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'psw2Label'});
+var citta=new LiveValidation('citta',{
+   validMessage: ' ', onlyOnBlur: true,insertAfterWhatNode:'cittaLabel'});
 
 nome.add(Validate.Presence, {failureMessage:"Campo vuoto"});
 
 cognome.add(Validate.Presence, {failureMessage:"Campo vuoto"});
 
-dBorn.add(Validate.Presence, {
-   failureMessage:"Vuoto"});
-dBorn.add( Validate.Numericality, day);
+//~ dBorn.add(Validate.Presence, {
+   //~ failureMessage:"Vuoto"});
+//~ dBorn.add( Validate.Numericality, day);
 
-mBorn.add(Validate.Presence, {failureMessage:"Vuoto"});
-mBorn.add( Validate.Numericality, month);
+//~ mBorn.add(Validate.Presence, {failureMessage:"Vuoto"});
+//~ mBorn.add( Validate.Numericality, month);
 
-yBorn.add(Validate.Presence, {failureMessage:"Vuoto"});
-yBorn.add( Validate.Numericality, year);
+//~ yBorn.add(Validate.Presence, {failureMessage:"Vuoto"});
+//~ yBorn.add( Validate.Numericality, year);
 
-dDrive.add(Validate.Presence, {failureMessage:"Vuoto"});
-dDrive.add( Validate.Numericality, day);
+//~ dDrive.add(Validate.Presence, {failureMessage:"Vuoto"});
+//~ dDrive.add( Validate.Numericality, day);
 
-mDrive.add(Validate.Presence, {failureMessage:"Vuoto"});
-mDrive.add( Validate.Numericality, month);
+//~ mDrive.add(Validate.Presence, {failureMessage:"Vuoto"});
+//~ mDrive.add( Validate.Numericality, month);
 
-yDrive.add(Validate.Presence, {failureMessage:"Vuoto"});
-yDrive.add( Validate.Numericality, year);
+//~ yDrive.add(Validate.Presence, {failureMessage:"Vuoto"});
+//~ yDrive.add( Validate.Numericality, year);
 
 email.add(Validate.Presence, {failureMessage:"Campo vuoto"});
 email.add(Validate.Email, {failureMessage: "Indirizzo non valido"});
@@ -69,7 +73,7 @@ citta.add(Validate.Presence, {failureMessage:"Campo vuoto"});
 setFormFocus("iscrizione");
 
 function validaIscr() {
-   var areAllValid = LiveValidation.massValidate( [  nome, cognome,email,dBorn,mBorn,yBorn,dDrive,mDrive,yDrive,user,psw,psw2,citta ] );
+   var areAllValid = LiveValidation.massValidate( [  nome, cognome,email,user,psw,psw2,citta]);//,email,dBorn,mBorn,yBorn,dDrive,mDrive,yDrive,user,psw,psw2,citta ] );
     is = document.getElementById("iscrizioneForm"); 
     
    if (areAllValid) {
